a4380fc1e3deaba006242accb40bd651998ad13c
abbasarg <abbasalhassanj@gmail.com>
2025-06-26 01:26:30 +0100

    Update Config.js

--- Config.js
+++ Config.js
@@ -1,19 +1,19 @@
 // hey there, kindly read this file till the end for better configuration
 
 global.settings = {
-  OWNER_NUMBERS: "2349067339193", // your number with country code
-  OWNER_NAME: 'Miracle', // your owner name 
-  BOT_NAME: "KORD", // your preferred bot name
-  SESSION_ID: 'your_sessionid', // replace your_sessionid with yours and make sure its inside rhe quotes
+  OWNER_NUMBERS: "2349043883888", // your number with country code
+  OWNER_NAME: 'Abbas_Cyber', // your owner name 
+  BOT_NAME: "CYBER_BOT", // your preferred bot name
+  SESSION_ID: 'kord_ai-y1QkXDH5JkYTQ0XA', // replace your_sessionid with yours and make sure its inside rhe quotes
   INVINCIBLE_MODE: false, // toogle invincible mode 
-  WORK_MODE: "Public",
+  WORK_MODE: "private",
   TIME_ZONE: "Africa/Lagos",
   ANTI_VIEWONCE: true,
   READ_ALL_MESSAGES: false,
   AUTO_READ_STATUS: true,
   PREFIX: [".", "/", "!"],
   GROUP_EVENTS: true,
-  WELCOME_MESSAGE: "Hello! I'm KORD-AI, your friendly WhatsApp bot.",
+  WELCOME_MESSAGE: "Hello! I'm CYBER BOT, your friendly WhatsApp bot.",
   DEFAULT_TRANSLATION_LANG: "en",
   COMMAND_COOLDOWN_TIME_IN_MS: 0.01,
   MAX_COMMANDS_PER_MINUTE: 100,
@@ -21,9 +21,9 @@ global.settings = {
   AUTO_LIKE_STATUS: true,
   AUTO_LIKE_EMOJI: '🍁',
   BANKING_DETAILS: {
-    bank: "Kord-Ai Bank", // the name of your preferred bank
-    acc_number: "1234567890", // account number for your preferred bank
-    acc_name: "MyAccountName" // name of your bank
+    bank: "OPAY", // the name of your preferred bank
+    acc_number: "8104029966", // account number for your preferred bank
+    acc_name: "Abbas Alhassan" // name of your bank
   },
   /* You can also replace the numbers with your addresses if you're using crypto, a command for this will come soon. */
   API_KEYS: {
